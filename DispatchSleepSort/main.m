f5f12bd43f61d45ed4ed484fedea619b193c608f
ANNotunzdY <takayoshi@pankaku.com>
2011-05-27 16:54:49 +0900

    Ver. 1.0

--- DispatchSleepSort/main.m
+++ DispatchSleepSort/main.m
@@ -12,10 +12,34 @@ int main (int argc, const char * argv[])
 {
 
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-	// insert code here...
-	NSLog(@"Hello, World!");
-
+	dispatch_semaphore_t s = dispatch_semaphore_create(0);
+	__block int remain = argc - 1;
+	NSMutableArray* result = [NSMutableArray arrayWithCapacity:remain];
+	
+	for (int i=1; i<argc; i++) {
+		int n = atoi(argv[i]);
+		
+		double delayInSeconds = n;
+		dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+		dispatch_after(popTime, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
+			NSLog(@"%d", n);
+			[result addObject:[NSNumber numberWithInt:n]];
+			remain--;
+			if (!remain) {
+				dispatch_semaphore_signal(s);
+			}
+		});
+	}
+	
+	if (argc == 1) {
+		dispatch_semaphore_signal(s);
+	}
+	
+	dispatch_semaphore_wait(s, DISPATCH_TIME_FOREVER);
+	dispatch_release(s);
+	
+	NSLog(@"Result: %@", [result description]);
+	
 	[pool drain];
     return 0;
 }
