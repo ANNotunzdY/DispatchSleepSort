1c5f4a8ded6802a0a06e9d73c5d27cdb005e07bf
ANNotunzdY <takayoshi@pankaku.com>
2011-05-27 15:00:30 +0900

    Initial Commit

---
+++ DispatchSleepSort/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DispatchSleepSort
+//
+//  Created by ANNotunzdY on 11/05/27.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
